a0640ffb8a7d244dd83c70ec742c854ddc28e5df
Anton Lindstrom <lindztr@gmail.com>
2017-09-07 08:53:45 +0200

    Add email notifications

---
+++ bin/.local/bin/email_notifier.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# Based on: https://bbs.archlinux.org/viewtopic.php?pid=962423#p962423
+# This version is simplified a lot and uses a hardcoded mailbox.
+
+import pyinotify
+import gi
+
+gi.require_version('Notify', '0.7')
+
+from gi.repository import Notify
+from os.path import expanduser
+from mailbox import MaildirMessage
+from email.header import decode_header
+
+maildir_folder = expanduser(r"~/.mail/")
+new_mail_folder = maildir_folder+"antonlindstrom.com/INBOX/new"
+notification_timeout = 12000
+
+Notify.init(r'email_notifier.py')
+
+def newfile(event):
+    fd = open(event.pathname, 'r')
+    mail = MaildirMessage(message=fd)
+    msg = "New mail from " + str(mail['From'])
+    n = Notify.Notification.new("You've got mail!", msg, "mail-unread-new")
+    fd.close()
+    n.set_timeout(notification_timeout)
+    n.show()
+
+wm = pyinotify.WatchManager()
+notifier = pyinotify.Notifier(wm, newfile)
+
+wm.add_watch(new_mail_folder, pyinotify.IN_CREATE | pyinotify.IN_MOVED_TO)
+
+notifier.loop()
