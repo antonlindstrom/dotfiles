4d657982300c9f48c181b42f34f08fbf0512aafa
Anton Lindström <lindztr@gmail.com>
2019-02-15 14:58:20 +0100

    fish: Default GO111MODULE on

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,4 +6,6 @@ set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 
 set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git --ignore vendor/ -g ""'
 
+set -x GO111MODULE on
+
 gpg-connect-agent updatestartuptty /bye >/dev/null
