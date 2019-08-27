53842c3e321ca29c8e33713eda1d999c46b6d22a
Anton Lindström <lindztr@gmail.com>
2019-08-27 11:50:16 +0200

    fish: Add GOPROXY env variable

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,5 +7,6 @@ set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 set -x FZF_DEFAULT_COMMAND 'ag --hidden --ignore .git --ignore vendor/ -g ""'
 
 set -x GO111MODULE on
+set -x GOPROXY https://proxy.golang.org
 
 gpg-connect-agent updatestartuptty /bye >/dev/null
