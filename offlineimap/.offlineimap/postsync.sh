2a30479309e225c012187d5a8dddc204b7bd6546
Anton Lindström <lindztr@gmail.com>
2019-10-23 13:10:21 +0200

    email: Improve attachment script
    This complicates the script a bit to store some attachments. The change
    makes sure that a timestamp is stored in front of the attachment to have
    multiple with the same name but sent at different times.

--- offlineimap/.offlineimap/postsync.sh
+++ offlineimap/.offlineimap/postsync.sh
@@ -3,7 +3,23 @@
 notmuch new
 notmuch tag --batch --input=/home/anton/.offlineimap/notmuch_tags
 
-# Save the payslips with tags payslip and work in a separate directory.
-notmuch search --format=text0 --output=files tag:payslip and tag:work | \
-    xargs -0 -I {} --no-run-if-empty ripmime --overwrite -i {} -d ~/documents/payslips/; \
-    rm ~/documents/payslips/{textfile*,*.jpg}
+TEMP_DIR=$(mktemp -d)
+TARGET="$HOME/documents/payslips/"
+
+payslips=$(notmuch search --format=text0 --output=files tag:payslip | xargs -0 -I {} --no-run-if-empty echo {})
+
+for email in $payslips; do
+	datefile=$(mktemp)
+	cat "$email" | grep Date: | cut -d' ' -f 2- > "$datefile"
+	prefix=$(date -f "$datefile" "+%Y%m%d_%H%M")
+
+	ripmime -i "$email" -d "$TEMP_DIR"
+
+	for file in "$TEMP_DIR"/*.pdf; do
+		b=$(basename "$file")
+		mv "$file" "$TEMP_DIR/${prefix}_$b" 2>/dev/null
+	done
+
+	rsync -a --include='*.pdf' --exclude='*' "$TEMP_DIR/" "$TARGET"
+	rm -r "$TEMP_DIR"
+done
